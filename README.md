bbdfe16e0ebbd885437a85cfc1e5e677f25efb5c
Alexis Van Hoorde <a.vanhoorde@etu-webschoolfactory.fr>
2020-04-21 12:33:29 +0200

    fix

--- README.md
+++ README.md
@@ -7,25 +7,17 @@ Be sure that you are in the good directory :
 `cd venv`
 
 Be sure that venv is install : 
-
 `python3.6 -m venv venv`
 
 Start the virtual environment with this command : 
-
 `source venv/bin/activate`
 
 In the virtual env : 
 
-
-
+Then, if you want to extract the data into a json file :
+`scrapy crawl aswxp -o quotes.json`
 
 ________________________________________
 
 
-response.xpath("//div[@class='workxp_one']").get()
-// Toute la div avec les work experience 
 
-workxp = response.xpath("//div[@class='workxp_one']")
-// assigne à une variable 
-
-workxp[0].css(".workxp_company_date > a::text").getall()
--- venv/demo/demo/spiders/aswxp.py
+++ venv/demo/demo/spiders/aswxp.py
@@ -1,19 +1,25 @@
 import scrapy
 
+# Création de la class aswxp (pour Adley Salabi Work Experiences)
 class aswxp(scrapy.Spider):
     name = "aswxp"
-
+    # On renseigne l'url du portfolio 
     start_urls = ["http://adleysalabi.com//"]
-
+    # On parse la réponse 
     def parse(self, response):
-            self.logger.info('hello this is my first spider')
+        # self.logger afin d'avooir un retour visuel que la spider à bien été lancée
+            self.logger.info('Everything seems to be working :) ')
+            # On décide de définir la class .workxp_one dans une variable 
             workxps = response.css('div.workxp_one')
+            # Puis on boucle sur toutes les div .workxp_one
             for workxp_one in workxps:
+                # On renseigne chaque élément souhaité à l'aide de sélecteurs css
                 yield {
                     'job': workxp_one.css('.workxp_job::text').get(),
                     'contrat': workxp_one.css('.workxp_type::text').get(),
                     'dates': workxp_one.css('.workxp_company_date.workxp_date::text').get(),
-                    'company': workxp_one.css('.workxp_company_date>a::text').get(),
+                    'company_name': workxp_one.css('.workxp_company_date>a::text').get(),
+                    'company_url': workxp_one.css('.workxp_company_date>a::attr(href)').get(),
                     'resume': workxp_one.css('p::text').get(),
-                    'skills': workxp_one.css('.workxp_skills_wrapper.workxp_skill_used::text').get(),
+                    'skills': workxp_one.css('.workxp_skills_wrapper').get(),
                 }
